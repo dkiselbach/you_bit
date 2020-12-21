8eb2f1f5f2b8b1cd9b3c1a57fe6b36a3562d3b7d
dkiselbach <dylankiselbach@gmail.com>
2020-12-20 18:09:42 -0800

    Remove graphiql as it was throwing off heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@
 Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
   # mount_graphql_devise_for 'User', at: 'graphql_auth'
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
