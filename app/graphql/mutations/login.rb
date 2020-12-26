ebe0fd248b5fe30d3035c45ef1f11cbe98f11c0f
dkiselbach <dylankiselbach@gmail.com>
2020-12-26 09:26:01 -0500

    Add login

--- app/graphql/mutations/login.rb
+++ app/graphql/mutations/login.rb
@@ -2,15 +2,62 @@
 
 module Mutations
   # Class responsible for logging in the user. This is a child class of the graphql devise gem.
-  class Login < GraphqlDevise::Mutations::Login
+  class Login < Mutations::BaseMutation
     description "Login a user with email and password. This will return the user object and the associated
  credentials for login."
-    field :user, Types::UserType, null: true, description: "Access the user's fields if login is successful."
+    argument :email,    String, required: true
+    argument :password, String, required: true
 
-    def resolve(**args)
-      user_resource = super
+    field :user, Types::UserType, null: false, description: "Access the user's fields if login is successful."
+    field :credentials,
+          Types::UserCredentialType,
+          null: false,
+          description: 'Authentication credentials for the user. Null if after signUp resource is not active for
+ authentication (e.g. Email confirmation required).'
 
-      user_resource.merge(user: user_resource[:authenticatable])
+    def resolve(email:, password:)
+      resource = find_resource(
+        :email,
+        get_case_insensitive_field(:email, email)
+      )
+
+      if resource && active_for_authentication?(resource)
+        if invalid_for_authentication?(resource, password)
+          raise_user_error(I18n.t('graphql_devise.sessions.bad_credentials'))
+        end
+
+        new_headers = set_auth_headers(resource)
+        controller.sign_in(:user, resource, store: false, bypass: false)
+
+        yield resource if block_given?
+
+        { user: resource, credentials: new_headers }
+      elsif resource && !active_for_authentication?(resource)
+        if locked?(resource)
+          raise_user_error(I18n.t('graphql_devise.mailer.unlock_instructions.account_lock_msg'))
+        else
+          raise_user_error(I18n.t('graphql_devise.sessions.not_confirmed', email: resource.email))
+        end
+      else
+        raise_user_error(I18n.t('graphql_devise.sessions.bad_credentials'))
+      end
+    end
+
+    private
+
+    def invalid_for_authentication?(resource, password)
+      valid_password = resource.valid_password?(password)
+
+      (resource.respond_to?(:valid_for_authentication?) && !resource.valid_for_authentication? { valid_password }) ||
+        !valid_password
+    end
+
+    def active_for_authentication?(resource)
+      !resource.respond_to?(:active_for_authentication?) || resource.active_for_authentication?
+    end
+
+    def locked?(resource)
+      resource.respond_to?(:locked_at) && resource.locked_at
     end
   end
 end
