b16c170503182fd68e96ae482c48cbb39d838141
dkiselbach <dylankiselbach@gmail.com>
2020-12-30 15:06:01 -0500

    Add current and longest streak queries. Add most_recent scope to HabitLog.

--- app/models/habit_log.rb
+++ app/models/habit_log.rb
@@ -2,5 +2,6 @@ class HabitLog < ApplicationRecord
   validates :habit_type, habit_type: true, presence: true
   validates :logged_date, date: true, presence: true
   belongs_to :habit
+  scope :most_recent, -> { order('logged_date DESC').first }
 
 end
