c067117dcf44db9960320e89163a8d7aee980036
dkiselbach <dylankiselbach@gmail.com>
2021-01-13 08:50:46 -0800

    Update namespacing to work with Zeitwerk

--- app/services/push_notification.rb
+++ app/services/push_notification.rb
@@ -1,30 +1,28 @@
-module Services
-  # Class for sending push notifications to the RN App.
-  class PushNotification
-    attr_accessor :handler
-    attr_reader :client, :token, :sound, :body, :title
+# Class for sending push notifications to the RN App.
+class PushNotification
+  attr_accessor :handler
+  attr_reader :client, :token, :sound, :body, :title
 
-    def initialize(**args)
-      @client = Exponent::Push::Client.new
-      @token = "ExponentPushToken[#{args[:token]}]"
-      @title = args[:habit_name]
-      @sound = 'default'
-      @body = args[:body]
-    end
+  def initialize(**args)
+    @client = Exponent::Push::Client.new
+    @token = "ExponentPushToken[#{args[:token]}]"
+    @title = args[:habit_name]
+    @sound = 'default'
+    @body = args[:body]
+  end
 
-    def send
-      message = {
-        to: token,
-        sound: sound,
-        title: title,
-        body: body
-      }
+  def send
+    message = {
+      to: token,
+      sound: sound,
+      title: title,
+      body: body
+    }
 
-      self.handler = client.send_messages(message)
-    end
+    self.handler = client.send_messages(message)
+  end
 
-    def verify_delivery
-      client.verify_deliveries(handler.receipt_ids)
-    end
+  def verify_delivery
+    client.verify_deliveries(handler.receipt_ids)
   end
 end
