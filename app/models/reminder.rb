7533c26ec8a7ad239af9a76c1461740aa00680c4
dkiselbach <dylankiselbach@gmail.com>
2021-02-03 14:58:05 -1000

    Messing around with time zones...

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -5,7 +5,8 @@ class Reminder < ApplicationRecord
   validates :remind_at, date: true, presence: true
   validates :time_zone, time_zone: true, presence: true
   belongs_to :habit
-  after_save :enqueue_reminders
+  after_save :set_reminder
+  attr_accessor :scheduler
 
   DAYS_OF_WEEK = %w[sunday monday tuesday wednesday thursday friday saturday].freeze
 
@@ -13,7 +14,14 @@ class Reminder < ApplicationRecord
     Reminder.all.find_each(&:enqueue_reminders)
   end
 
-  def enqueue_reminders
+  def set_reminder
+    enqueue_reminders(scheduler: 'new_reminder')
+  end
+
+  private
+
+  def enqueue_reminders(scheduler: 'daily_scheduler')
+    self.scheduler = scheduler
     return unless habit.active && (habit.daily? || habit.frequency.include?(weekday))
 
     remind_at = reminder_time
@@ -24,17 +32,17 @@ class Reminder < ApplicationRecord
     end
   end
 
-  private
-
   def reminder_time
+    # scheduler is set for everyday at 12:00 AM UTC. Use UTC for the date unless the method was
+    # called from a new reminder being created.
+    date = scheduler == 'daily_scheduler' ? Time.now.in_time_zone('UTC') : Time.now.in_time_zone(time_zone)
     Time.zone = time_zone
-    date = Time.now.utc
     time = remind_at.in_time_zone
     reminder_time = Time.zone.local(date.year, date.month, date.day, time.hour, time.min, time.sec)
     reminder_time > Time.current ? reminder_time : nil
   end
 
   def weekday
-    DAYS_OF_WEEK[Time.now.utc.wday]
+    scheduler == 'daily_scheduler' ? DAYS_OF_WEEK[Time.now.in_time_zone('UTC').wday] : DAYS_OF_WEEK[Time.now.in_time_zone(time_zone).wday]
   end
 end
